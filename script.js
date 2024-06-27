43b6b666f7c2fc01920270ff32642352d8724305
Karthikeyan Ramesh <karthikramesh444@gmail.com>
2024-06-27 16:59:57 +0530

    todo list done with local storage

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    // owl carousel 
     $(".owl-carousel").owlCarousel({
         loop: true,
         dots: false,
@@ -6,16 +8,19 @@ $(document).ready(function () {
     });
 
 
+    // image gallery 
     document.querySelectorAll('.gallery-image').forEach(function(element) {
         element.addEventListener('click', function() {
             element.classList.toggle('gallery');
         });
     });
 
+
     // infinite scroll 
     document.querySelectorAll('.marquee-item').forEach((item, i) => {
         item.style.setProperty('--n', i + 1);
     });
+
 
     // accordian 
     document.querySelectorAll('.accordion-item').forEach((element) => {
@@ -29,10 +34,47 @@ $(document).ready(function () {
         });
     });
 
-
-    // to-to list 
-    let input = document.getElementById('task');
-    let button = document.getElementById('add-btn');
-    let todolist = document.getElementById('todo-list');
 });
 
+
+// todo list 
+let button = document.getElementById('add');
+let input = document.getElementById('input');
+let todolist = document.getElementById('todolist');
+
+button.addEventListener('click', () => {
+    if (input.value === '') {
+        alert("Enter valid task...")
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = input.value;
+        todolist.appendChild(li);
+
+        let span = document.createElement('span');
+        span.innerHTML = "<i class='bx bx-x f20'></i>"
+        li.appendChild(span)
+    }
+    input.value = '';
+    saveData();
+})
+
+todolist.addEventListener('click', function (element) {
+    if (element.target.tagName === 'LI') {
+        element.target.classList.toggle('checked');
+        saveData();
+    }
+    else if (element.target.tagName === 'I') {
+        element.target.parentElement.parentElement.remove();
+        saveData();
+    };
+});
+
+function saveData() {
+    localStorage.setItem('data', todolist.innerHTML )
+};
+
+function showData() {
+    todolist.innerHTML = localStorage.getItem('data')
+};
+
+showData();
